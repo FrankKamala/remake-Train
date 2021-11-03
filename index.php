7582998ad2b320d9a232218b8a4d71a2ff2b5717
FrankKamala <frankkamala@gmail.com>
2021-11-03 16:28:55 +0300

    link buttons

--- index.php
+++ index.php
@@ -34,8 +34,8 @@
       
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
         
-        <button type="button" class="btn btn-primary btn-lg px-4" href="">InterCounty Train</button>
-        <button type="button" class="btn btn-dark btn-lg px-4" href="">Local Train</button>
+        <button type="button" class="btn btn-primary btn-lg px-4" href="interCounty/interBook.php">InterCounty Train</button>
+        <button type="button" class="btn btn-dark btn-lg px-4" href="local/localbook.php">Local Train</button>
       </div>
     </div>
   </div>
