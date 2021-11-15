09b2e2077e687922b8cb909fc88ec17455e82e65
FrankKamala <frankkamala@gmail.com>
2021-11-15 16:17:19 +0300

    add train avilable detail Item finish

--- interCounty/intercountyDetail.php
+++ interCounty/intercountyDetail.php
@@ -11,25 +11,33 @@
    
     <!-- <link href="styles/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" > -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="inter.css">
+    <link rel="stylesheet" href="detailP.css">
     <title>Bookings Available</title>
 </head>
 <body>
-<div class="container">
-  <div class="row">
-    <div class="col-sm-"><P>Train 1</P></div>
+    <div class="available">  
+    <div class="container">
+  <div class="row-md">
+    <div class="col" id="UIName"><P>Madaraka InterCounty</P></div>
     <div class="col">
         <div class ="row">
         <P>From : <span class="from">Nairobi</span>          To:<span class="to">Mombasa</span> </P>
+
+
+        <P>Departure : <span>10:00</span>   Arrival:<span>3:00</span> </P>
+
+        <div class="col">
+
+        <div class="row mbali"><button type="button" class="btn btn-outline-primary">Book Now</button></div>
+        </div>
         </div>
         
-        <div class ="row">
-        <P>Departure : <span>10:00</span>   Arrival:<span>3:00</span> </P>
-        </div>
+        
     <div class="w-100"></div>
    
-    <div class="col"><P>Book Now</P></div>
-  </div>
+   
+  </div>  </div>
+
 </div>
 </body>
 </html>
