4699d9188ac17843553a4f45f36ec22614c82979
FrankKamala <frankkamala@gmail.com>
2021-11-15 14:46:18 +0300

    Add detail page struct

---
+++ interCounty/intercountyDetail.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   
+    <!-- <link href="styles/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" > -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="inter.css">
+    <title>Bookings Available</title>
+</head>
+<body>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-"><P>Train 1</P></div>
+    <div class="col">
+        <div class ="row">
+        <P>From : <span class="from">Nairobi</span>          To:<span class="to">Mombasa</span> </P>
+        </div>
+        
+        <div class ="row">
+        <P>Departure : <span>10:00</span>   Arrival:<span>3:00</span> </P>
+        </div>
+    <div class="w-100"></div>
+   
+    <div class="col"><P>Book Now</P></div>
+  </div>
+</div>
+</body>
+</html>
