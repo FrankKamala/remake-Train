e25dcd61699d8a1dcf9885dc3207c40d2f454360
FrankKamala <frankkamala@gmail.com>
2021-12-01 18:11:14 +0300

    edt

--- interCounty/book/interBook.php
+++ interCounty/book/interBook.php
@@ -1,6 +1,13 @@
 <?php
 
-require_once '../../db/db_con.php';
+// require_once '../../db/db_con.php';
+
+$link = mysqli_connect("localhost", "root", "", "swift_train");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
     $fullnames =  $_REQUEST['fullnames'];
         $idpassport = $_REQUEST['idPass'];
        
@@ -11,20 +18,19 @@ require_once '../../db/db_con.php';
           
         // Performing insert query execution
         // here our table name is bookings
-        $sql = "INSERT INTO bookings  VALUES ('$fullnames', 
-            '$idpassport','$ticket_numbers','$coachType'";
+        // $sql = "INSERT INTO bookings  VALUES ('$fullnames', 
+        //     '$idpassport','$ticket_numbers','$coachType','Mombasa', 'Nairobi', '08:00:00.875000', '14:00:00.596000', '2021-12-10'";
 
-            // // INSERT INTO `bookings` (`pass_name`, 
-            // `pass_IDPASS`, `ticket_numbers`, 
-            // `ticket_type`, 
-            // `train_from`, `train_to`, `time_dept`, `time_arr`, `day_travel`) VALUES ( 'Karanja Kim', '43434343', '5', 'VIP', 'Mombasa', 'Nairobi', '08:00:00.875000', '14:00:00.596000', '2021-12-10')
+$sql = " INSERT INTO `bookings` (`pass_name`, 
+`pass_IDPASS`, `ticket_numbers`, 
+`ticket_type`
+) VALUES ( '$fullnames', '$idpassport', '$coachType', '$coachType')";
+
+            
 
 
- $rs = mysqli_query($connection, $sql);
-if($rs)
-{
-	echo "Records Inserted";
-}
+            if(mysqli_query($link, $sql)){
+                echo "Records added successfully.";}
 
 else
 {
